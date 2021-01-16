e87687c87ea9123a075cbba18f95c56b552c6993
Monir H <monirhfy@gmail.com>
2021-01-17 00:56:22 +0600

    Testimonials V1

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+(function ($) {
+    $('#year').text(new Date().getFullYear());
+
+    $(document).ready(function () {
+        // Testimonials V1
+        $('.testimonials-v1').slick({
+            infinite: true,
+            autoplay: false,
+            arrows: true,
+            pauseOnHover: false,
+            autoplaySpeed: 2000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: '<div class="user-prev"><i class="fas fa-long-arrow-alt-left"></i></div>',
+            nextArrow: '<div class="user-next"><i class="fas fa-long-arrow-alt-right"></i></div>',
+        });
+    });
+})(jQuery);
