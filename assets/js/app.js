4847f534d5214854dbc4a93cadfc1ee62c163ab4
Monir H <monirhfy@gmail.com>
2021-01-21 14:17:08 +0600

    Testimonials V3

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,10 @@
             autoplaySpeed: 2000,
             slidesToShow: 1,
             slidesToScroll: 1,
-            prevArrow: '<div class="user-prev"><i class="fas fa-long-arrow-alt-left"></i></div>',
-            nextArrow: '<div class="user-next"><i class="fas fa-long-arrow-alt-right"></i></div>',
+            prevArrow:
+                '<div class="user-prev"><i class="fas fa-long-arrow-alt-left"></i></div>',
+            nextArrow:
+                '<div class="user-next"><i class="fas fa-long-arrow-alt-right"></i></div>',
         });
         // Testimonials V2
         $('.testimonials-v2').slick({
@@ -23,8 +25,45 @@
             autoplaySpeed: 2000,
             slidesToShow: 1,
             slidesToScroll: 1,
-            prevArrow: '<div class="user-prev"><i class="fas fa-long-arrow-alt-left"></i></div>',
-            nextArrow: '<div class="user-next"><i class="fas fa-long-arrow-alt-right"></i></div>',
+            prevArrow:
+                '<div class="user-prev"><i class="fas fa-long-arrow-alt-left"></i></div>',
+            nextArrow:
+                '<div class="user-next"><i class="fas fa-long-arrow-alt-right"></i></div>',
+        });
+
+        // Testimonials V3
+
+        $('.testimonials-v3__controls').slick({
+            infinite: true,
+            autoplay: false,
+            arrows: false,
+            cssEase: 'linear',
+            pauseOnHover: false,
+            autoplaySpeed: 2000,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            asNavFor: '.testimonials-v3__quotes',
+            centerMode: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3,
+                    },
+                },
+            ],
+        });
+        $('.testimonials-v3__quotes').slick({
+            infinite: true,
+            dots: true,
+            autoplay: false,
+            arrows: false,
+            pauseOnHover: false,
+            autoplaySpeed: 2000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            asNavFor: '.testimonials-v3__controls ',
         });
     });
 })(jQuery);
